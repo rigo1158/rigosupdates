01b15e351fe7d071007328a8c63f09cd187dda90
Rigo Cortez <rigocortez@Rigos-MBP.search.charter.net>
2024-03-12 00:44:46 -0400

    another version of scripting

--- startup_files/script.js
+++ startup_files/script.js
@@ -1,11 +1,8 @@
-function sayHello() {
-    console.log("hello");
+function mathMe() {
+    var a = prompt("enter first number");
+    var b = prompt("enter second number");
+    var answer = a * b;
+    return alert(answer);
 }
 
-sayHello();
-
-var sayBye = function() {
-    console.log("bye");
-}
-
-sayBye();
+mathMe();
